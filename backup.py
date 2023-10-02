10abe8d6a32b2a5afba8cd3d78dce2b7eeee44d9
Artem Gerus <gerus.kz@gmail.com>
2023-10-02 22:34:51 +0300

    Add All photos from VK and Backup in Google Drive

--- backup.py
+++ backup.py
@@ -1,9 +1,18 @@
-import requests, os, json
+from __future__ import print_function
+
+import requests, os, json, sys
 from tqdm import tqdm
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets
 from PyQt5.QtGui import QIcon
 from design import Ui_MainWindow
 from time import sleep
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
+
 
 # Работает кривовато, но я пытался :)
 class Ui(QtWidgets.QMainWindow):
@@ -27,7 +36,7 @@ class Ui(QtWidgets.QMainWindow):
     def start_backup(self):
         token = self.ui.lineEdit.text()
         vk_id = self.ui.lineEdit_2.text()
-        self.backup(token, vk_id)
+        print(self.backup(token, vk_id))
 
     def progres_start(self):
         self.ui.label_3.setText("Загрузка")
@@ -50,8 +59,8 @@ class VK:
     # Постоянные параметры ссылки
     def _get_common_params(self):
         return {
-            'access_token': 'vk1.a.Rsu9XTmQYUi2rjMBzk9CmpNlzZE8YcYtl4_pi4a6wYbZxCKG6wRksgU4mr_Ge_7bss-CezSArw3VXbAFWLg3FBQ88NRdqkJnLHx2fbk3SDxDIY8EqEG8WirTL5C6udNDNWZwHdqieeQ30bylC4TEALU-kInsBr-C4NSADtiBpF22BwfPO9fNDdlXKKXJCHut-CCgKyfXuV1bvfjHVpb2sw',
-            'v': '5.154'
+            'access_token': 'vk1.a.qb_pYaRfkQQP80ICdJ7D6WJtyXPtuaQhHVWmBMhk_UxNPAG31x5W9JBIGgNzbUFfXoSUo1UOJtiCb7XR0EhE1xxj2DPYg0ntAjUbsJkTYJUKiq8jYXce-sIfTJkLKzNUbkR-_nT9mtBnnHPwjjHzqLp8MavXPttHCZ17BeGsVHVVx1jokkOt1V3iEF9o-RkK',
+            'v': '5.150'
         }
 
     # Получаем словарь со всей информацией о фотографиях профиля
@@ -59,12 +68,11 @@ class VK:
         params = self._get_common_params()
         params.update({
             'owned_id': self.id_user,
-            'album_id': 'profile',
             'extended': 1,
             'photo_sizes': 1,
         })
         response = (requests.get
-                    (f'{self.BASE_URL}photos.get?', params=params))
+                    (f'{self.BASE_URL}photos.getAll?', params=params))
         return response.json()
 
     # Получаем нужную нам информацию о фотографиях для дальнейших манипуляций
@@ -99,7 +107,7 @@ class YandexDisk:
         for photo in self.info_photos.values():
             response = requests.get(photo['link'])
             name_file = str(photo["likes"])
-            file = f'C:\\Users\Steve\Desktop\courseworks\coursework_oop_api\PhotosLibery\\{name_file}.jpg'
+            file = f'{os.getcwd()}\PhotosLibery\\{name_file}.jpg'
             if os.path.isfile(file):
                 name_file += f"_{photo['date']}"
             with open(f'PhotosLibery\{name_file}.jpg', 'bw') as f:
@@ -132,31 +140,99 @@ class YandexDisk:
         return response
 
 
-TOKEN = 'y0_AgAAAAA3TrhxAADLWwAAAADssFkYbT5QEHQIRreFqBdntuVlj4TNIRc'
+class GoogleDrive:
+    def __init__(self):
+        # If modifying these scopes, delete the file token.json.
+        self.SCOPES = ['https://www.googleapis.com/auth/drive.file']
+        self.creds = None
 
+    def auth(self):
+        """Shows basic usage of the Drive v3 API.
+        Prints the names and ids of the first 10 files the user has access to.
+        """
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.json'):
+            self.creds = Credentials.from_authorized_user_file('token.json', self.SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not self.creds or not self.creds.valid:
+            if self.creds and self.creds.expired and self.creds.refresh_token:
+                self.creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', self.SCOPES)
+                self.creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(self.creds.to_json())
+        return self.creds
 
-# print(backup(440529550, TOKEN))
+    def create_folder(self):
+        try:
+            # create drive api client
+            service = build('drive', 'v3', credentials=self.auth())
+            file_metadata = {
+                'name': 'BackupImages',
+                'mimeType': 'application/vnd.google-apps.folder'
+            }
+
+            # pylint: disable=maybe-no-member
+            file = service.files().create(body=file_metadata, fields='id'
+                                          ).execute()
+            # print(F'Folder ID: "{file.get("id")}".')
+            return file.get('id')
+
+        except HttpError as error:
+            print(F'An error occurred: {error}')
+            return None
+
+    def upload_photos(self, folder_id, name_photo):
+        try:
+            # create drive api client
+            service = build('drive', 'v3', credentials=self.auth())
+
+            file_metadata = {
+                'name': name_photo,
+                'parents': [folder_id]
+            }
+            media = MediaFileUpload('web.png',
+                                    mimetype='image/png', resumable=True)
+            # pylint: disable=maybe-no-member
+            file = service.files().create(body=file_metadata, media_body=media,
+                                          fields='id').execute()
+            # print(F'File ID: "{file.get("id")}".')
+            return file.get('id')
+
+        except HttpError as error:
+            print(F'An error occurred: {error}')
+            return None
+
 
 def backup(token, vk_id):
     result = []
     vk_client = VK(vk_id)
+    drive_client = GoogleDrive()
     yandex_client = YandexDisk(token, vk_client.get_information_vk_photos())
     yandex_client.get_photos_on_directory()
     yandex_client.create_folder()
-    path = 'C:\\Users\Steve\Desktop\courseworks\coursework_oop_api\PhotosLibery'
+    folder_id = drive_client.create_folder()
+    path = f'{os.getcwd()}\PhotosLibery'
     dir_list = os.listdir(path)
     for photo in tqdm(dir_list, ncols=85,
                       position=0, unit=' photos', desc='Loading'):
         yandex_client.upload_photo(yandex_client.get_url(photo), photo)
+        drive_client.upload_photos(folder_id, photo)
         result.append({'file_name': photo, 'size': 'z'})
     with open('result.json', 'w') as f:
         json.dump(result, f, indent=2)
+    for photo in dir_list:
+        os.remove(os.path.join(path, photo))
     return len(vk_client.get_information_vk_photos())
 
 
 if __name__ == '__main__':
-    import sys
-
+    # backup('y0_AgAAAAA3TrhxAADLWwAAAADssFkYbT5QEHQIRreFqBdntuVlj4TNIRc', 440529550)
     app = QtWidgets.QApplication([])
     w = Ui()
     w.show()
